a63e3114e05b27be1e7b4d5556f72823fa1c9536
Sebastián <sebastianmaciel@outlook.com>
2020-11-21 12:23:00 -0300

    Comentarios y correcciones

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ $('.botonBuscarUsuario').on('click', () => {
 
 // La función que hace la búsqueda con manejo de errores
 let buscarDataParaEl = (usuarioDeGitHub) => {
+
   // Hacemos un Ajax...
   $.ajax({
     url: `https://api.github.com/users/${usuarioDeGitHub}`,
     context: document.body,
+    
     // Si sale todo bien, le mandamos la info del usuario a la función que llena el HMTL
     success: function (dataDelUsuario) {
       popularHTMLConLa(dataDelUsuario)
@@ -52,13 +54,14 @@ let buscarDataParaEl = (usuarioDeGitHub) => {
 // Esta función llena el HTML con la data correspondiente, tanto datos de usuario como mensajes de error.
 let popularHTMLConLa = (dataDelUsuario) => {
 
-  // Desectructuramos un toque:
-  let { name: nombre, 
-        bio: biografia,
-        public_repos: repositorios,
-        public_gists: gists,
-        followers,
-        avatar_url: fotoDePerfil } = dataDelUsuario;
+  // Desestructuramos un toque:
+  let { name: nombre,               // Sebastián Maciel
+        bio: biografia,             // Half-Blood Developer
+        public_repos: repositorios, // 11
+        public_gists: gists,        // 12
+        followers,                  // 13
+        avatar_url: fotoDePerfil }  // Foto de Perfil
+        = dataDelUsuario;
 
   // Mandamos al HTML la data final
   $('#nombre').text(nombre)
@@ -66,5 +69,5 @@ let popularHTMLConLa = (dataDelUsuario) => {
   $('#repositorios').text(repositorios)
   $('#gists').text(gists)
   $('#followers').text(followers)
-  $('#fotoDePerfil').attr('src', fotoDePerfil)
+  $('#fotoDePerfil').attr('src', fotoDePerfil) // Llenamos el atributo "src" de la imagen con la url que nos da GitHub
 }
