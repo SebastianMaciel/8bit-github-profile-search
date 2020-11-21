774355ca962d635f6cb515c71458c4630f0151f5
Sebastián <sebastianmaciel@outlook.com>
2020-11-21 12:01:15 -0300

    Comentarios!

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ $('.buscarUsuario').on('click', () => {
   buscarData(usuarioGitHub)
 })
 
+// La función que hace la búsqueda con manejo de errores
 let buscarData = (usuarioGitHub) => {
+  // Hacemos un Ajax...
   $.ajax({
     url: `https://api.github.com/users/${usuarioGitHub}`,
     context: document.body,
+    // Si sale todo bien, le mandamos la info del usuario a la función que llena el HMTL
     success: function (data) {
       popularHTML(data)
     },
+    // Si para algo malo, igual llenamos el HTML...
     error:function (xhr, ajaxOptions, thrownError){
+      // Este error, es cuando no se encuentra el usuario
       if(xhr.status==404) {
         let noData = {
           avatar_url: "https://avatars1.githubusercontent.com/u/15474343?s=400&v=4",
@@ -24,6 +29,7 @@ let buscarData = (usuarioGitHub) => {
           bio: ""
         }
           popularHTML(noData)
+      // Esto sucede cuando github te pone el límite de consultas por usuario...
       }
       if(xhr.status==403) {
         let noData = {
@@ -40,6 +46,7 @@ let buscarData = (usuarioGitHub) => {
   })
 }
 
+// Esta función llena el HTML con la data correspondiente, tanto datos de usuario como mensajes de error.
 let popularHTML = (data) => {
   $('#nombre').text(data.name)
   $('#bio').text(data.bio)
